7517e85c9ee6bc26bb85e94e27623b81b6017a33
Rodion Spiridonov <rodionspriridonov14@gmail.com>
2024-10-14 20:24:32 +0200

    Adding some changes with NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,37 @@
+"use client"
+
 import Footer from "@/components/Footer/Footer";
 import MusclesChart from "@/components/MusclesChart/MusclesChart";
 import NavBar from "@/components/NavBar/NavBar";
+import Navigation from "@/types/Navigaton";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Main({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  
+  const [navigation, setNavigation] = useState<Navigation[]>([
+    { name: 'Muscle Chart', href: '/', current: true },
+    { name: 'About', href: '/about', current: false },
+    { name: 'Contact', href: '/contact', current: false },
+    { name: 'FAQ', href: '/faq', current: false },
+  ]);
+
+  useEffect(() => {
+    if (pathname) {
+      setNavigation((prevNav) =>
+        prevNav.map((item) => ({
+          ...item,
+          current: pathname === item.href,
+        }))
+      );
+    }
+  }, [pathname]);
 
   return (
     <div className="flex flex-col min-h-screen">
       <header>
-        <NavBar />
+        <NavBar navigation={navigation} />
       </header>
       <main className="flex-grow m-auto mt-8 w-1/2">
         {children || <MusclesChart />}
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
                     <li className="text-gray-500 hover:text-gray-300"><i><a href="https://github.com/rodionspi/training_helper">Check out the code on GitHub</a></i></li>
                  </ul>
                 <ul className="list-none bg-gray-700 rounded-md p-3">
-                    <li><a href="#" className="hover:text-slate-400">Muscle Chart</a></li>
+                    <li><a href="/" className="hover:text-slate-400">Muscle Chart</a></li>
                     <li><a href="/about" className="hover:text-slate-400">About</a></li>
                     <li><a href="/contact" className="hover:text-slate-400">Contact</a></li>
                     <li><a href="/faq" className="hover:text-slate-400">FAQ</a></li>
