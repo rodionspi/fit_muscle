240387ac891597be5edb790b42a4adf970617206
Rodion Spiridonov <rodionspriridonov14@gmail.com>
2024-10-14 20:24:50 +0200

    Adding "About" Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import Layout from "../page";
+import Main from "../page";
 
 const About = () => {
     return (
-        <Layout>
-            About...
-        </Layout>
+        <Main>
+            <h1 className="text-4xl font-bold">About Trainer App</h1>
+            <br />
+            <h2 className="text-2xl font-medium">What can this app help you with?</h2>
+            <br />
+            <p>This app will help you figure out how to progress faster and more effectively in muscle growth and strength gains. You can find the best training program that suits you. You can also choose from muscle displays such as a table, graph, and more.</p>
+            <br />
+            <h2 className="text-2xl font-medium">How can you help this app grow?</h2>
+            <br />
+            <p>I would be glad if you could share this application with colleagues, friends, relatives and so on. You can also send me donations if you want to contribute to the development of this application (the card number is attached in my github, see below)</p>
+            <br />
+            <h2 className="text-2xl font-medium">Who is working on the website development?</h2>
+            <br />
+            <p>My <a href="https://github.com/rodionspi" className="text-sky-400">github</a>. I am a developer with two years of experience. I have experience with JavaScript and Typescript languages. If you want to ask me something or you are interested in something, write to me at my <a href="rodionspirik48@gmail.com" className="text-sky-400">e-mail</a></p>
+        </Main>
     )
 }
 
