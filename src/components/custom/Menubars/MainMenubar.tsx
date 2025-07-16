660f6cb18ffaba9ba8e8fb877284bf7036513de6
Rodion Spiridonov <rodionspriridonov14@gmail.com>
2025-07-16 23:24:33 +0200

    finished the page header for phones

--- src/components/custom/Menubars/MainMenubar.tsx
+++ src/components/custom/Menubars/MainMenubar.tsx
@@ -37,15 +37,15 @@ const MainMenubar = ({ navigation }: MainMenubarComponentProps) => {
                 <div className="space-y-1 px-2 pb-3 pt-2">
                     {navigation.map((item) => (
                     <Link href={item.href} key={item.name}>
-                    <MenubarItem
-                        aria-current={item.current ? 'page' : undefined}
-                        className={
-                        (item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white') +
-                        ' block rounded-md px-3 py-2 text-base font-medium'
-                        }
-                    >
-                        {item.name}
-                    </MenubarItem>
+                        <MenubarItem
+                            aria-current={item.current ? 'page' : undefined}
+                            className={
+                            (item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white') +
+                            ' block rounded-md px-3 py-2 text-base font-medium'
+                            }
+                        >
+                            {item.name}
+                        </MenubarItem>
                     </Link>
                     ))}
                 </div>
--- src/components/custom/Menubars/UserMenubar.tsx
+++ src/components/custom/Menubars/UserMenubar.tsx
@@ -1,7 +1,8 @@
-import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "@radix-ui/react-menubar";
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
 import { User } from 'lucide-react';
 import React from 'react';
 import { useUser } from '@/contexts/UserContext';
+import Link from "next/dist/client/link";
 
 const UserMenubar = () => {
     const {userData, setUserData} = useUser();
@@ -19,36 +20,37 @@ const UserMenubar = () => {
                     <User/>
                 </MenubarTrigger>
                 <MenubarContent
-                    className="z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                    className="sm:hidden"
                 > 
-                    {userData? (
-                    <>
-                        <MenubarItem>
-                        <a href={`/profile/${userData.id}`} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                            Profile
-                        </a>
-                        </MenubarItem>
-                        <MenubarItem>
-                        <a href="/" onClick={handleSignOut} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                            Sign out
-                        </a>
-                        </MenubarItem>
-                    </>
-                    ) : (
-                    <>
-                        <MenubarItem>
-                        <a href="/profile/login" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                            Login
-                        </a>
-                        </MenubarItem>
-                        <MenubarItem>
-                        <a href="/profile/registration" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                            Registration
-                        </a>
-                        </MenubarItem>
-                    </>
-                    )}
-                    
+                    <div className="space-y-1 px-2 pb-3 pt-2">
+                        {userData? (
+                            <>
+                                <Link href={`/profile/${userData.id}`}>
+                                    <MenubarItem className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'>
+                                        Profile
+                                    </MenubarItem>
+                                </Link>
+                                <Link href="/">
+                                    <MenubarItem className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'>
+                                        Sign out
+                                    </MenubarItem>
+                                </Link>
+                            </>
+                            ) : (
+                            <>
+                                <Link href="/profile/login">
+                                    <MenubarItem className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'>
+                                        Login
+                                    </MenubarItem>
+                                </Link>
+                                <Link href="/profile/registration">
+                                    <MenubarItem className='text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium'>
+                                        Registration
+                                    </MenubarItem>
+                                </Link>
+                            </>
+                        )}
+                    </div>
                 </MenubarContent>
             </MenubarMenu>
         </Menubar>
