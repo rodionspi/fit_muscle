804ee0c2505455e6296a6a0aab26d8b4cd475343
Rodion Spiridonov <rodionspriridonov14@gmail.com>
2025-08-14 12:18:35 +0200

    Remove firebaseConfig.js and related environment files from .gitignore

---
+++ firebaseConfig.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjAbtk_TxSMSi2GUww4aIMVPpsZReVAsQ",
+  authDomain: "fit-muscle-8d2f0.firebaseapp.com",
+  projectId: "fit-muscle-8d2f0",
+  storageBucket: "fit-muscle-8d2f0.firebasestorage.app",
+  messagingSenderId: "780023014682",
+  appId: "1:780023014682:web:83215c7ac94d7f000ab8a3",
+  measurementId: "G-N7HBJDD28Q"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+// Configure Google Auth Provider
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+
+export { auth, provider, db, signInWithPopup };
