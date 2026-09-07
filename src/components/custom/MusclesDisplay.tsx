cc37f0a02943016dacaf1e49040f7819a638fd16
Rodion <rodionspiridonov14@gmail.com>
2026-09-07 13:35:19 +0200

    style: update button variants and text size in MusclesDisplay and Tabs components for improved consistency

--- src/components/custom/MusclesDisplay.tsx
+++ src/components/custom/MusclesDisplay.tsx
@@ -33,11 +33,11 @@ const MusclesDisplay = () => {
               </TabsList>
 
               <div className="flex gap-2 w-auto justify-center sm:justify-start">
-                <Button variant="outline" size="sm" className="text-slate-300 border-slate-700">
+                <Button variant="secondary" size="sm" className="border-slate-700 text-md">
                   <Info size={16} className="mr-2" />
                   Beginner Guide
                 </Button>
-                <Button variant="outline" size="sm" className="text-slate-300 border-slate-700">
+                <Button variant="secondary" size="sm" className="border-slate-700 text-md">
                   <Calendar size={16} className="mr-2" />
                   Workout Planner
                 </Button>
