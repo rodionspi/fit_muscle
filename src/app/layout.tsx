5111085ddc274bd86b52d31fa76d8a7dc0f77389
Rodion <rodionspiridonov14@gmail.com>
2026-09-23 22:31:13 +0200

    refactor: remove react-router dependencies and simplify layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import "./globals.css";
 import { UserProvider } from "../contexts/UserContext";
-import { BrowserRouter } from "react-router-dom";
 import { MusclesProvider } from "@/contexts/MusclesContext";
 
 export default function RootLayout({
@@ -22,18 +21,9 @@ export default function RootLayout({
         />
       </head>
       <body className=" text-zinc-100 antialiased">
-        {typeof window !== "undefined" ? (
-          <BrowserRouter
-          >
-            <UserProvider>
-              <MusclesProvider>{children}</MusclesProvider>
-            </UserProvider>
-          </BrowserRouter>
-        ) : (
-          <UserProvider>
-            <MusclesProvider>{children}</MusclesProvider>
-          </UserProvider>
-        )}
+        <UserProvider>
+          <MusclesProvider>{children}</MusclesProvider>
+        </UserProvider>
       </body>
     </html>
   );
